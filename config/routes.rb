1352b547d9ae826a1c0386ded446f1c1a94ab79f
Michael Sive <mikesive@gmail.com>
2017-07-20 12:42:53 -0700

    add index route for battle

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :users, only: [:create, :update]
   resource :sessions, only: [:create]
-  resources :battles, only: [:new, :create, :show]
+  resources :battles, only: [:new, :create, :index, :show]
   resources :battle_pets, only: [:create, :index, :show]
 
   namespace :hooks do
